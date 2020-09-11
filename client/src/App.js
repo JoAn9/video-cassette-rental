bb5ba0663a4b926b43286ae744d5ea170a278742
JoAn <joannacalinska@gmail.com>
2020-09-11 10:33:27 +0200

    user context

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
   Switch,
   useHistory,
 } from 'react-router-dom';
-import { isLoggedIn, logout } from './auth';
+import { isLoggedIn, logout, getLoggedUser } from './utils/auth';
 import ActorDetail from './ActorDetail';
 import LoginForm from './LoginForm';
 import MovieBoard from './MovieBoard';
@@ -13,42 +13,51 @@ import MovieDetail from './MovieDetail';
 import MovieForm from './MovieForm';
 import ActorForm from './ActorForm';
 import NavBar from './NavBar';
+import Chat from './Chat';
+import UserContext from './contexts/UserContext';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(isLoggedIn());
+  const [user, setUser] = useState(getLoggedUser());
+
   let history = useHistory();
 
   const handleLogin = () => {
     setLoggedIn(true);
+    setUser(getLoggedUser());
     history.push('/');
   };
 
   const handleLogout = () => {
     logout();
+    setUser(null);
     setLoggedIn(false);
     history.push('/');
   };
 
   return (
-    <div>
-      <NavBar loggedIn={loggedIn} onLogout={handleLogout} />
-      <section className="section">
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={MovieBoard} />
-            <Route exact path="/actors/new" component={ActorForm} />
-            <Route path="/actors/:actorId" component={ActorDetail} />
-            <Route exact path="/movies/new" component={MovieForm} />
-            <Route path="/movies/:movieId" component={MovieDetail} />
-            <Route
-              exact
-              path="/login"
-              render={() => <LoginForm onLogin={handleLogin} />}
-            />
-          </Switch>
-        </div>
-      </section>
-    </div>
+    <UserContext.Provider value={user}>
+      <div>
+        <NavBar loggedIn={loggedIn} onLogout={handleLogout} />
+        <section className="section">
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={MovieBoard} />
+              <Route exact path="/actors/new" component={ActorForm} />
+              <Route path="/actors/:actorId" component={ActorDetail} />
+              <Route exact path="/movies/new" component={MovieForm} />
+              <Route path="/movies/:movieId" component={MovieDetail} />
+              <Route exact path="/Chat" component={Chat} />
+              <Route
+                exact
+                path="/login"
+                render={() => <LoginForm onLogin={handleLogin} />}
+              />
+            </Switch>
+          </div>
+        </section>
+      </div>
+    </UserContext.Provider>
   );
 }
 
