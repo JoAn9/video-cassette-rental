f0f08f774dc127457098b886aabd990f7c7806ae
JoAn <joannacalinska@gmail.com>
2020-10-05 11:16:56 +0200

    types for ts and schema updated

---
+++ client/src/types.ts
@@ -0,0 +1,44 @@
+export type ID = string;
+
+export interface Actor {
+  id: ID;
+  name: string;
+  description: string;
+  movies: MovieDetail[];
+}
+
+export interface ActorData {
+  actor: Actor;
+}
+
+export interface ActorVars {
+  id: ID;
+}
+
+export interface ActorsData {
+  actors: Actor[];
+}
+
+export interface ActorInput {
+  name: string;
+  description: string;
+}
+
+export interface MovieDetail {
+  id: ID;
+  title: string;
+  description: string;
+  actor?: Actor;
+}
+
+export interface MovieDetailData {
+  movie: MovieDetail;
+}
+
+export interface MovieDetailVars {
+  id: ID;
+}
+
+export interface MoviesData {
+  movies: MovieDetail[];
+}
