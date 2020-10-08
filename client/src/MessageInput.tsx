1baa9c4eb29ecded3e9721bd0ca2752fb7c7ab8d
JoAn <joannacalinska@gmail.com>
2020-10-08 20:27:51 +0200

    MessageInput - refactor to tsx

---
+++ client/src/MessageInput.tsx
@@ -0,0 +1,36 @@
+import React, { ReactElement, useState } from 'react';
+
+type MessageProps = {
+  onSend: Function;
+};
+
+function MessageInput({ onSend }: MessageProps): ReactElement {
+  const [message, setMessage] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onSend(message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <div className="box">
+      <p className="control">
+        <input
+          className="input"
+          value={message}
+          type="text"
+          placeholder="Say something..."
+          onChange={e => handleChange(e)}
+          onKeyPress={handleKeyPress}
+        />
+      </p>
+    </div>
+  );
+}
+
+export default MessageInput;
